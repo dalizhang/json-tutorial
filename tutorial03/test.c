1d4aa60451d90f50db3e78360e7c85c21151b9b4
dalizhang <dalizhang@foxmail.com>
2017-12-06 17:53:31 +0800

    t03p02

--- tutorial03/test.c
+++ tutorial03/test.c
@@ -107,10 +107,10 @@ static void test_parse_number() {
 static void test_parse_string() {
     TEST_STRING("", "\"\"");
     TEST_STRING("Hello", "\"Hello\"");
-#if 0
+//#if 0
     TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
     TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
-#endif
+//#endif
 }
 
 #define TEST_ERROR(error, json)\
